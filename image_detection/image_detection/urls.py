f68124ceeda123fa8d3c305c7ba30dd9f7c68250
anasgfdi <anas.gfdi@yahoo.fr>
2022-10-20 10:07:04 +0200

    creation homepage

--- image_detection/image_detection/urls.py
+++ image_detection/image_detection/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from .views import homepage
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',homepage, name='home')
 ]
